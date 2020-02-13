fe968ac51b9e02262b22d7e8c095a82f7ff2d743
jennysueee <56518268+jennysueee@users.noreply.github.com>
2020-02-12 19:53:44 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,33 +1,40 @@
-// UFO Sightings - Javascript
 
-var tableData = data;
 let tbody = d3.select("tbody");
+// From data.js
+var tableData = data;
 
-// Set up the table
+// Build table
 function buildTable(data){
+    
     tbody.html("");
+     
     data.forEach((dataRow) => {
+        
         let row = tbody.append("tr");
+        
         Object.values(dataRow).forEach((val) => {
+            
             let cell = row.append("td");
             cell.text(val);
         });
     })
 }
-
-buildtable(tableData);
-
-// Event that Triggers a Function When the Button is Clicked
-function filterTable(){
+// Event trigger
+function handleClick(){
+    
     d3.event.preventDefault();
-
+    
     let date = d3.select("#datetime").property("value");
     let filterData = tableData;
 
+    // Filter by date;
     if(date) {
         filterData = filterData.filter((row) => row.datetime === date);
     }
+    
     buildTable(filterData);
 }
 
-d3.selectAll("#filter-btn").on("click", filterTable);
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+buildTable(tableData);
